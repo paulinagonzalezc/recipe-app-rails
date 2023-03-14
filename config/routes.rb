b6c597d5d4c47fbe25ada6f2ea751451ec7271c5
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-03-14 13:47:52 -0400

    Set up devise for login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :foods, only: [:index, :new, :create, :destroy]
+
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
+  
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
 end
