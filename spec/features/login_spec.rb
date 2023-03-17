2b8d06757a04490f32ff8d261cb5236b9fd10e1a
momotsow <kgomotsomkansi@gmail.com>
2023-03-17 16:25:26 +0200

    Add features spec tests

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "Logins", type: :feature do
+  it 'can enter a name and receive a greeting' do
+    visit new_user_registration_path
+    expect(page).to have_content 'Sign up'
+  end
+
+  it 'click the login error' do
+    visit 'users/sign_up'
+    click_button 'Sign up'
+    expect(page).to have_content "Email can't be blank"
+  end
+
+  it 'click the login error' do
+    User.create(name: 'Hello', email: 'test@gmail.com', password: 'password')
+    visit 'users/sign_up'
+    fill_in 'user[email]', with: 'test@gmail.com'
+    fill_in 'user[password]', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_content "Name can't be blank"
+  end
+end
