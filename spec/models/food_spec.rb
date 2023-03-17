19a70b6c10fbc1dc02c2b8e0a8d089150f35efc4
momotsow <kgomotsomkansi@gmail.com>
2023-03-17 16:25:58 +0200

    Add models spec tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,46 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  let(:user) { User.create(name: 'Donard', email: 'example@mail.com', password: 'password') }
-  let(:food) { Food.create(user_id: user.id, name: 'Apple', measurement_unit: 'kg', price: 12) }
+  create_food = Food.new(name: 'mango', measurement_unit: 'kilograms', price: 10)
+  subject { create_food }
 
-  describe 'Validations' do
-    context 'when valid' do
-      it { expect(food).to be_valid }
-    end
+  before { subject.save }
 
-    it 'should allow valid name' do
-      food.name = 'Egg'
-      expect(food).to_not be 'Apple'
-    end
+  it 'name should not be blank' do
+    subject.name = 'mango'
+    expect(subject.name).to eq 'mango'
+  end
+  it 'measurement_unit field should not be blank' do
+    subject.measurement_unit = 'kilograms'
+    expect(subject.measurement_unit).to eq 'kilograms'
+  end
 
-    it 'should allow valid name' do
-      food.name = 'Apple'
-      expect(food).to be_valid
-    end
-
-    it 'should validate measurment unit' do
-      food.measurement_unit = nil
-      expect(food).to_not be_valid
-    end
-
-    it 'should validate measurment unit' do
-      food.measurement_unit = 'kg'
-      expect(food).to be_valid
-    end
-
-    it 'should validate price' do
-      food.price = -5
-      expect(food).to_not be_valid
-    end
-
-    it 'should validate price' do
-      food.price = 0
-      expect(food).to_not be_valid
-    end
-
-    it 'should validate price' do
-      food.price = 6
-      expect(food).to be_valid
-    end
+  it 'price must be an integer greater than or equal to 0' do
+    subject.price = 3
+    expect(subject.price).to be >= 0
+  end
 end
